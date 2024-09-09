a74a2ed3d0f59b4f7e15257d15c2f66e42fae21f
Himanshu Raj <himanshurajkush@gmail.com>
2024-09-09 19:14:29 +0530

    Add decryption support
    ref: https://github.com/nebrassy/device_oneplus_sm86xx-common-TWRP

--- device.mk
+++ device.mk
@@ -24,9 +24,20 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
 
+# Recovery libs
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion 
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
+
 # Platform 
 PLATFORM_VERSION := 99.87.36
 PLATFORM_SECURITY_PATCH := 2127-12-31
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
